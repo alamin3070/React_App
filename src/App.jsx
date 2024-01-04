40a045c8cf759a9c0012a79ac88afb61a2fba7fe
alamin3070 <alamin3070@gmail.com>
2024-01-04 19:23:21 +0600

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 function App() {
   return (
     <>
-    <h1 className='text-center 3xl'> Welcome to React App Deployment </h1>
+    <h1 className='text-center text-3xl'> Welcome to React App Deployment </h1>
     </>
   );
 }
