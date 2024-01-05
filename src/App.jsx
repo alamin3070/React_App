490e048ddaa6fb04d8c38a8b55ca9d65430771e2
alamin3070 <alamin3070@gmail.com>
2024-01-05 09:35:10 +0600

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Card from './components/Card';
 function App() {
   return (
     <>
     <h1 className='text-center text-3xl'> Welcome to React App Deployment </h1>
+    <Card/>
     </>
   );
 }
